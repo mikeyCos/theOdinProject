95deebef348adf311bc89cdf459fac453e245a01
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-03-10 16:52:35 -0800

    Completed tempConversion exercise.

--- foundations/javaScript/javascript-exercises/07_tempConversion/tempConversion.js
+++ foundations/javaScript/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
 // °C = (°F - 32) * (5 / 9) 
-
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
 const convertToFahrenheit = function(celsius) {
 // °F = (°C * (9 / 5)) + 32
-
+  return Math.round(((celsius * (9 / 5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
