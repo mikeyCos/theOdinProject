efffb5ea49dede98deac1344d812a4be9aa66048
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-03-14 08:54:59 -0700

    Removed playGame, removed return statements and reverted to using console.log. RPS-UI Branch.

--- foundations/rock-paper-scissors/javascript.js
+++ foundations/rock-paper-scissors/javascript.js
@@ -1,70 +1,33 @@
-// 6 MAR 2023
-let playerSelection;
-let computerSelection;
+// 06 MAR 2023
+// 14 MAR 2023
+let playerChoice = prompt('enter rock, paper or scissors');
+playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1, playerChoice.length).toLowerCase();
 
-let createDiv;
-let createText;
-let createHeader;
-
-
-console.log(playerSelection);
-
-function getComputerChoice() {
+function getComputerChoice(callBack) {
+    // debugger;
     let randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
     if (randomNumber === 0) {
-        return 'Rock'; // if number is = 0, then computer chooses rock
+        computerChoice = 'Rock'; // if number is = 0, then computer chooses rock
     } else if (randomNumber === 1) {
-        return 'Paper'; // if number is = 1, then computer chooses paper
+        computerChoice = 'Paper';// if number is = 1, then computer chooses paper
     } else {
-        return 'Scissors'; // if number is = 2, then computer chooses scissors
+        computerChoice = 'Scissors'; // if number is = 2, then computer chooses scissors
     }
+    playRound(playerChoice, computerChoice);
 }
 
 // function for 1 game of rock-paper-scissors
 // function takes 2 parameters, playerSelection and computerSelection
-    // if player loses return "You Lose! ${computerSelection} beats ${playerSelection}"
-    // if computer loses return "You Win! ${playerSelection beats ${computerSelection}"
-    // if playerSelection === computerSelection, return "Tie game"
-function playRound(playerSelection, computerSelection) {
-        // console.log('playerSelection: ' + playerSelection); //  For debugging purposes only
-        // console.log('computerSelection: ' + computerSelection); //  For debugging purposes only
-        // playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1, playerSelection.length).toLowerCase();
-        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1, computerSelection.length).toLowerCase();
-        if ((playerSelection === 'Rock') && (computerSelection === 'Paper') || (playerSelection === 'Paper') && (computerSelection === 'Scissors') || (playerSelection === 'Scissors') && (computerSelection === 'Rock')) {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`;
-        } else if ((playerSelection === 'Paper') && (computerSelection === 'Rock') || (playerSelection === 'Scissors') && (computerSelection === 'Paper') || (playerSelection === 'Rock') && (computerSelection === 'Scissors')) {
-            return `You Win! ${playerSelection} beats ${computerSelection}`;
+function playRound(player, computer) {
+        debugger;
+        if ((player === 'Rock') && (computer === 'Paper') || (player === 'Paper') && (computer === 'Scissors') || (player === 'Scissors') && (computer === 'Rock')) {
+            console.log(`You lose`);
+        } else if ((player === 'Paper') && (player === 'Rock') || (player === 'Scissors') && (computer === 'Paper') || (player === 'Rock') && (computer === 'Scissors')) {
+            console.log(`You win`);
         } else {
-            return `Tie game`;
+            console.log(`Tie game`);
         }
 }
 
-// function that plays 5 rounds
-function playGame() {
-    for(i = 1; i <= 5; i++) {
-        playerSelection = prompt("Please enter your selection.");
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1, playerSelection.length).toLowerCase();
-        while (playerSelection !== 'Rock' && playerSelection !== 'Paper' && playerSelection !== 'Scissors') {
-            playerSelection = prompt("Type in either 'rock', 'paper', or 'scissors'.");
-            playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1, playerSelection.length).toLowerCase();
-        }
-        computerSelection = getComputerChoice();
-        console.log('This is round ' + i + '\n' + playRound(playerSelection, computerSelection));
-
-        createHeader = document.createElement("h2");
-        document.body.appendChild(createHeader);
-        createText = document.createTextNode(`This is round ${i}.`);
-        createHeader.appendChild(createText);
-
-        createDiv = document.createElement("div");
-        document.body.appendChild(createDiv);
-        createText = document.createTextNode(playRound(playerSelection, computerSelection));
-        createDiv.appendChild(createText);
-        if (i === 5) {
-            alert(`Game completed!`)
-            console.log('Game completed.');
-        }
-    }
-}
-
-console.log(playGame());
+getComputerChoice(playRound);
