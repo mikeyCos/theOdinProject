eb7c79ac8e7884d00ac99837ecdcc301120db76c
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-03-14 11:40:11 -0700

    3 Separate functions redone for targeting elements Not able to play 1 round without calling each function, except for playerChoice.

--- foundations/rock-paper-scissors/javascript.js
+++ foundations/rock-paper-scissors/javascript.js
@@ -1,33 +1,46 @@
 // 06 MAR 2023
 // 14 MAR 2023
-let playerChoice = prompt('enter rock, paper or scissors');
-playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1, playerChoice.length).toLowerCase();
+const buttons = document.querySelectorAll('button');
+let computerChoice;
+let playerChoice;
+const results = document.querySelector('.results');
 
-function getComputerChoice(callBack) {
-    // debugger;
-    let randomNumber = Math.floor(Math.random() * 3);
-    let computerChoice;
-    if (randomNumber === 0) {
-        computerChoice = 'Rock'; // if number is = 0, then computer chooses rock
-    } else if (randomNumber === 1) {
-        computerChoice = 'Paper';// if number is = 1, then computer chooses paper
-    } else {
-        computerChoice = 'Scissors'; // if number is = 2, then computer chooses scissors
-    }
-    playRound(playerChoice, computerChoice);
+function getPlayerChoice() {
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerChoice = button.className;
+            playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1, playerChoice.length).toLowerCase();
+        });
+    });
 }
 
-// function for 1 game of rock-paper-scissors
-// function takes 2 parameters, playerSelection and computerSelection
-function playRound(player, computer) {
-        debugger;
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    computerChoice = 'Scissors';
+    if (randomNumber === 0) {
+        computerChoice = 'Rock'; // if number is = 0, then computer chooses rock
+        // console.log(computerChoice);
+    } else if (randomNumber === 1) {
+        computerChoice = 'Paper';// if number is = 1, then computer chooses paper
+        // console.log(computerChoice);
+    } 
+}
+
+function playRound(player, computer) { 
+        let resultsText;
         if ((player === 'Rock') && (computer === 'Paper') || (player === 'Paper') && (computer === 'Scissors') || (player === 'Scissors') && (computer === 'Rock')) {
             console.log(`You lose`);
-        } else if ((player === 'Paper') && (player === 'Rock') || (player === 'Scissors') && (computer === 'Paper') || (player === 'Rock') && (computer === 'Scissors')) {
+            resultsText = document.createTextNode('You lose');
+            results.appendChild(resultsText);
+        } else if ((player === 'Paper') && (computer === 'Rock') || (player === 'Scissors') && (computer === 'Paper') || (player === 'Rock') && (computer === 'Scissors')) {
             console.log(`You win`);
-        } else {
+            resultsText = document.createTextNode('You win');
+            results.appendChild(resultsText);
+        } else if ((player === 'Paper') && (computer === 'Paper') || (player === 'Rock') && (computer === 'Rock') || (player === 'Scissors') && (computer === 'Scissors')) {
             console.log(`Tie game`);
+            resultsText = document.createTextNode('Tie game');
+            results.appendChild(resultsText);
         }
 }
 
-getComputerChoice(playRound);
+getPlayerChoice();
