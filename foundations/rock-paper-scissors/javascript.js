079885ae2622db37adbed9d4445e25ff7310e0cd
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-03-18 13:12:08 -0700

    Added reset button, hid info only anchor tags, somewhat cleaned code, and added more styles.

--- foundations/rock-paper-scissors/javascript.js
+++ foundations/rock-paper-scissors/javascript.js
@@ -3,18 +3,25 @@
 // This article helped me divide the addEventListener and a named function instead of using an anonymous function.
 // https://medium.com/@DavideRama/removeeventlistener-and-anonymous-functions-ab9dbabd3e7b
 
-// define computer score
 let computerScore = 0;
-// define player score
 let playerScore = 0;
-// initialize nodes for player and computer scores
 let playerScoreNode = document.querySelector('.player');
 let computerScoreNode = document.querySelector('.computer');
-// initialized nodes for showing who won current round/game
 let results = document.querySelector('.results');
-
 let buttons = document.getElementById('btn-container');
-buttons.addEventListener('click', getPlayerChoice, true);
+let resetButton = document.getElementById('btn-reset');
+resetButton.addEventListener('click', reset, true);
+
+// get user input
+let roundsForm = document.getElementById('enter-rounds');
+roundsForm.addEventListener('submit', getRounds);
+
+let roundsCounter = 0;
+let rounds = 0;
+
+function turnOn() {
+    buttons.addEventListener('click', getPlayerChoice, true);
+}
 
 function getPlayerChoice(e) {
     let playerChoice = e.target.className;
@@ -22,7 +29,6 @@ function getPlayerChoice(e) {
     checkWinner(playerChoice);
 }
 
-// get computer choice
 function getComputerChoice() {
     let number = Math.floor((Math.random() * 3));
     if (number == 0) {
@@ -34,48 +40,50 @@ function getComputerChoice() {
     }
 }
 
-// check winner
-// if player wins, increment score by 1
-// if computer wins, increment score by 1
-// display who won the round
 function checkWinner(playerChoice) {
     let computerChoice = getComputerChoice();
-    console.log('playerChoice: ' + playerChoice); // for debugging
-    console.log('computerChoice: ' + computerChoice); // for debugging
+    roundsCounter++
     if ((playerChoice == 'Rock' && computerChoice == 'Scissors') || (playerChoice == 'Paper' && computerChoice == 'Rock') || (playerChoice == 'Scissors' && computerChoice == 'Paper')) {
-        console.log('Player wins round!');
         playerScore++;
-        results.textContent = 'Player wins round!';
+        results.textContent = `Round ${roundsCounter}: Player wins round!`;
     } else if ((computerChoice == 'Rock' && playerChoice == 'Scissors') || (computerChoice == 'Paper' && playerChoice == 'Rock') || ( computerChoice == 'Scissors' && playerChoice == 'Paper')) {
-        console.log('Computer wins round!');
         computerScore++;
-        results.textContent = 'Computer wins round!';
+        results.textContent = `Round ${roundsCounter}: Computer wins round!`;
     } else {
-        console.log('Tie round!');
-        results.textContent = 'Tie round!';
+        results.textContent = `Round: ${roundsCounter}: Tie round!`;
     }
-    checkScore();
+    checkScore(rounds);
 }
 
-// check score
-// determines who wins the game at X amount of rounds
-// removes event listener(s)
-// display current score
 function checkScore() {
     playerScoreNode.textContent = `You: ${playerScore}`;
     computerScoreNode.textContent = `Computer: ${computerScore}`;
-    if (playerScore == 5 || computerScore == 5) {
+    if (roundsCounter == rounds) {
         buttons.removeEventListener('click', getPlayerChoice, true);
-        if (playerScore == 5) {
-            console.log(`🙂 Player wins the game! 🙂`);
+        rounds = 0;
+        if (playerScore > computerScore) {
             results.textContent = 'Player wins the game! ';
-        } else {
-            console.log(`Computer wins the game!`);
+        } else if (playerScore < computerScore) {
             results.textContent = '😥 Computer wins the game! 😥';
+        } else if (playerScore == computerScore) {
+            results.textContent = 'Tie game';
         }
     }
+}   
+
+// takes user input for how many rounds they want to play
+function getRounds (e) {
+    e.preventDefault();
+    rounds = document.getElementById('rounds').value;
+    checkScore();
+    turnOn();
 }
 
-// reset game
-// turn buttons back on
-// 
+function reset() {
+    turnOn();
+    playerScore = 0;
+    computerScore = 0;
+    roundsCounter = 0;
+    checkScore();
+    results.textContent = '';
+}
