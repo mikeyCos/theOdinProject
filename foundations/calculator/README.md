c228f404a77cf69c872ab8037676ea01d46800d8
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-04-08 10:12:00 -0700

    Implementing button click separately before merging with keydown cases.

--- foundations/calculator/README.md
+++ foundations/calculator/README.md
@@ -1,4 +1,5 @@
 View project here: <https://mikeycos.github.io/theOdinProject/foundations/calculator/index.html>  
+08 APR 2023: Implementing button functionality whilst mirroring case for keydown keys before merging both functions.  
 07 APR 2023: '1 + 2 =' returns 3, pressing 'Enter/=' again returns 5 (ie. 3 + 2). If user presses '+' then 'Enter/=' returns 10 (ie. 5 + 5), and if '+' then 'Enter/=' keys are pressed again, then 20 is returned (ie. 10 + 10). Now, if user presses only '=' then 30 is returned (ie. 20 + 10). Implemented subtraction/multiplication/division operations, delete key will revert all variables to initial values, testing backspace implementation, and repeating an operation after entering a number will perform the pressed operator. '2 + 3' then pressing '+' returns 5, and pressing '7' then '+' will return 12 (ie. 5 + 7 = 12).  
 06 APR 2023: Restarted project.  
 04 APR 2023: Able to store 2 numbers at a time, pressing '=' or 'Enter' after addition will continue to add the current sum by the original value added, and the sum replaces the first initial value.  
--- foundations/calculator/javascript.js
+++ foundations/calculator/javascript.js
@@ -1,30 +1,90 @@
 const resultsWindow = document.querySelector('.results-window');
+const buttons = document.querySelector('.buttons');
+buttons.addEventListener('click', verifyButton);
+// buttons.addEventListener('click', verifyKey);
 resultsWindow.textContent = 0;
 window.addEventListener('keydown', verifyKey);
+
 let check = false;
 let num1 = 0;
-let num2 = '';
+let num2 = 0;
 let number = '';
 let operator;
+
+function verifyButton(e) {
+    console.log(`Class name: ${e.target.className}`);
+    console.log(`Typeof e.target.className: ${typeof e.target.className}`);
+    console.log(`e.target.textContent: ${e.target.textContent}`);
+    console.log(`Typeof textContent: ${typeof e.target.textContent}`);
+    switch (e.target.textContent) {
+        case 'c':
+            num1 = 0;
+            num2 = 0;
+            number = '';
+            operator = '';
+            updateDisplay(num1);
+            break;
+        case 'backspace':
+            break;
+        case '.':
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            if (number !== '' && num2 !== '') {
+                operate(operator, num1, num2);
+                number = '';
+            }
+            check = true;
+            number = '';
+            operator = e.target.textContent;
+            break;
+        case '=':
+            if (number == '' && check) {
+                num2 = num1;
+            }
+            operate(operator, num1, num2);
+            number = '';
+            check = false;
+            break;
+        default:
+            if(isFinite(e.target.textContent)) {
+                number += e.target.textContent;
+                if(!check) {    
+                    num1 = number;
+                    num2 = '';
+                } else if (check) {
+                    num2 = number;
+                }
+                updateDisplay(number);
+            }
+    }
+}
 
 function verifyKey(e) {
     switch (e.key) {
         case 'Delete':
             num1 = 0;
-            num2 = '';
+            num2 = 0;
             number = '';
             operator = '';
             updateDisplay(num1);
             break;
         case 'Backspace':
-            //should not delete last answer
-            number = Number(number.toString().slice(0, -1));
-            if (!check) {
-                num1 = number;
+            number = number.toString().slice(0, -1);
+            if (number.length == 0) {
+                updateDisplay(0);
             } else {
-                num2 = number;
+                updateDisplay(number);
             }
-            updateDisplay(number);
+            break;
+        case '.':
+            if (number.indexOf('.') === -1) {
+                //if there is no decimal point
+                number += '.';
+                updateDisplay(number);
+            }
             break;
         case '+':
             // operator = 'add';
@@ -66,17 +126,22 @@ function verifyKey(e) {
                 if(!check) {    
                     num1 = number;
                     num2 = '';
-                    updateDisplay(num1);
+                    // updateDisplay(num1);
                 } else if (check) {
                     num2 = number;
-                    updateDisplay(num2);
+                    // updateDisplay(num2);
                 }
+                updateDisplay(number);
             }
     }
 }
 
 function updateDisplay(num) {
-    resultsWindow.textContent = num;
+    if (num === Infinity) {
+    resultsWindow.textContent = 'The answer is and will always be 42';
+    } else {
+        resultsWindow.textContent = num;
+    }
 }
 
 function operate(operator, a, b) {
@@ -118,6 +183,11 @@ function divide(a, b) {
 function square() {
     // square stuff
 }
-
-// clear
 // backspace
+
+// debugging in console
+// console.log(num1);
+// console.log(num2);
+// console.log(number);
+// console.log(check);
+// console.log(operator);
