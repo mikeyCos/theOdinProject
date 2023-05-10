9ca87d14d795c8e6f40ae34271a795dc86808008
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-05-10 13:16:14 -0700

    After merge

--- intermediate-html-and-css/projects/sign-up-form/javascript.js
+++ intermediate-html-and-css/projects/sign-up-form/javascript.js
@@ -2,60 +2,6 @@ let formItems = document.querySelectorAll('.form-item');
 
 for (let children of formItems) {
     for (let child of children.children) {
-<<<<<<< HEAD
-        if (child.tagName === 'INPUT') {
-            child.addEventListener('input', (e) => {
-                let validationId = e.target.getAttribute('aria-describedby');
-                let validationContainer = document.getElementById(validationId);
-                console.log('e.target.value:' + e.target.value);
-                console.log('test');
-                if (e.target.checkValidity()) {
-                    e.target.style.border = '2px solid #35b635';
-                    validationContainer.innerText = '';
-                } else {
-                    child.addEventListener('blur', function(e) {
-                        let isValid = e.target.validity.valid;
-                        let message;
-                        if (!isValid) {
-                            switch (e.target.getAttribute('id')) {
-                                case 'firstname':
-                                    message = 'Please enter your first name.';
-                                    e.target.setCustomValidity(message);
-                                    // only contains letters
-                                    // no numbers or symbols
-                                    break;
-                                case 'lastname':
-                                    message = 'Please enter your last name.';
-                                    // only contains letters
-                                    // no numbers or symbols
-                                    break;
-                                case 'email':
-                                    message = 'Please enter your email.'
-                                    // example, example@.a.nl
-                                    // 2 periods after one another is not allowed
-                                    // local-part max length of 64 characters
-                                    // domain-part max length of 63 characters
-                                    break;
-                                case 'phone':
-                                    message = 'Please enter your phone number.'
-                                    break;
-                                case 'password':
-                                    message = 'Please enter a password.'
-                                    break;
-                                case '':
-        
-                                    break;
-                                default: 
-                                    return;
-                            }
-                            e.target.style.border = '2px solid #FF0000';
-                            validationContainer.innerText = message;
-                        } else {
-                            validationContainer.innerText = '';
-                            e.target.style.border = '2px solid #35b635';
-                        }
-                    })
-=======
         if (child.tagName === 'INPUT' && child.id !== 'password-confirm') {
             let message;
             if (!child.checkValidity()) {
@@ -73,7 +19,6 @@ for (let children of formItems) {
                         message = 'Phone';
                     case 'password':
                         message = 'Password';
->>>>>>> sign-up-form-js
                 }
                 child.setCustomValidity(message);
             }
