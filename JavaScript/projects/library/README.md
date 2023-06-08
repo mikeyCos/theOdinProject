fc209a32ba6a1f5be58e07663dab8ea1beba510e
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-06-07 20:16:33 -0700

    Dialog element used with showModal() method. If dialog is open, dialog closes if e.traget.tagName === 'DIALOG'.

--- JavaScript/projects/library/README.md
+++ JavaScript/projects/library/README.md
@@ -3,4 +3,5 @@
 Live preview: (https://mikeycos.github.io/theOdinProject/JavaScript/projects/library/)
 ---
 ### Changelog
+7 JUN 2023 : Dialog element used with `showModal()` method, attached a click event to the dialog element when it is open to close dialog if `e.traget.tagName === 'DIALOG'`, `myLibrary` array will be manipulated on the DOM inside a div wrapper with a container inside and with a list element, and each key-value pair will be on their own 'row'.  
 6 JUN 2023 : Initiated skeleton HTML/CSS/JS files and linked files together.  
--- JavaScript/projects/library/javascript.js
+++ JavaScript/projects/library/javascript.js
@@ -1,12 +1,92 @@
+const buttonAddNewBook = document.querySelector('.add-book');
+
+const dialog = document.querySelector('dialog');
+const buttonCancel = document.querySelector('.cancel');
+const buttonAddBook = document.querySelector('.add');
+
+const formNode = document.querySelectorAll('#form');
+const form = formNode[0];
+
+// let library = document.querySelector('#library');
+
+buttonAddNewBook.addEventListener('click', () => {
+    dialog.showModal();
+    if(dialog.open) {
+        dialog.addEventListener('click', closeDialog);
+    }   
+})
+
+function closeDialog(e) {
+    console.log(e.target) //for debugging
+    if(e.target.tagName === 'DIALOG') {
+        dialog.close();
+        // do I need to removeEventListener?
+        // removeEventListener('click', closeDialog)
+    }
+}
+
+buttonCancel.addEventListener('click', () => {
+    dialog.close();
+})
+
 let myLibrary = []
 
-function Book() {
-    this.author = author
+function Book(title, author, pages, isbn) {
     this.title = title
+    this.author = author
     this.pages = pages
     this.isbn = isbn
+    myLibrary.push(this);
 }
 
 function addBookToLibrary() {
+    
+}
+
+function getInput() {
 
 }
+
+function displayBook() {
+    let library = document.querySelector('#library');
+    for(let i=0; i < myLibrary.length; i++) {
+        const book = document.createElement('div');
+        book.className = `book-${i+1}`;
+        library.appendChild(book);
+
+        const bookContainer = document.createElement('div');
+        bookContainer.className = `container`;
+        book.appendChild(bookContainer);
+        
+        const list = document.createElement('ul'); 
+        for (let key in myLibrary[i]) {
+            if (key === 'title') {
+                const title = document.createElement('h2');
+                title.textContent = myLibrary[i][key];
+                bookContainer.appendChild(title);
+
+                bookContainer.appendChild(list);
+            } else {
+                const listItem = document.createElement('li');
+                list.appendChild(listItem);
+
+                const bookInfo = document.createElement('p');
+                bookInfo.textContent = key;
+                listItem.appendChild(bookInfo);
+
+                const bookInfoValue = document.createElement('p');
+                bookInfoValue.textContent = myLibrary[i][key];
+                listItem.appendChild(bookInfoValue);
+            }
+            console.log(key + ' : ' + myLibrary[i][key]);
+        }
+    }
+    //loop through the array 'myLibrary'
+    // display books on the page
+}
+
+// testing manual input
+const bookOne = new Book(`1984`, `George Orwell`, 328, 9780451524935);
+const bookTwo = new Book (`The Hitchhiker's Guide to the Galaxy`, `Douglas Adams`, 224, 9780345391803);
+
+displayBook();
