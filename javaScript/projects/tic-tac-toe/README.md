6318fc7790c33c8bd95437523f408823f25e0903
mikeyCos <109037087+mikeyCos@users.noreply.github.com>
2023-06-26 18:01:27 -0700

    Worked on module pattern for the gameBoard. Module can render the current values of _gameboard array.

--- javaScript/projects/tic-tac-toe/README.md
+++ javaScript/projects/tic-tac-toe/README.md
@@ -1,6 +1,7 @@
 Tic Tac Toe
 ---
-Live preview (https://mikeycos.github.io/theOdinProject/javaScript/projects/library/)
+Live preview (https://mikeycos.github.io/theOdinProject/javaScript/projects/tic-tac-toe/)
 ---
 ### Changelog
+26 JUN 2023 : Worked on module pattern for the gameBoard, module can render the current values of `_gameboard[]`, if no index given for `_render(index, marker)` then the function creates and appends a text node, and `Array.from(_spaces).indexOf(e.target)` stores the index of the clicked element.  
 23 JUN 2023 : Skeleton HTML/CSS/JS files created.  
--- javaScript/projects/tic-tac-toe/index.js
+++ javaScript/projects/tic-tac-toe/index.js
@@ -1,9 +1,62 @@
 // module pattern
-const Gameboard = (() => {
-    const _gameboard = []
-})
+const ticTacToe = (function() {
+    //cache dom
+    const _board = document.querySelector('#gameboard');
+
+    let _gameboard = new Array(9);
+
+    //renders empty board
+    function _render(index, marker) {
+        if (!index) {
+            const ul = document.createElement('ul');
+            for (i = 0; i < _gameboard.length; i++) {
+                const li = document.createElement('li');
+                ul.appendChild(li);
+            }
+            _board.appendChild(ul);
+        } else {
+            // _spaces[index].textContent = marker;
+            const text = document.createTextNode(marker);
+            _spaces[index].appendChild(text);
+
+        }
+    }
+
+    _render();
+    //event listeners
+    const _spaces = _board.querySelectorAll('li');
+    _spaces.forEach(list => list.addEventListener('click', _markBoardSpace));
+
+    let counter = 0;
+    function _markBoardSpace(e) {
+        let marker;
+        if (counter % 2 === 0) {
+            marker = 'X';
+        } else {
+            marker = 'O';
+        }
+
+        //gets index of clicked elememt on the dom
+        const elementIndex = Array.from(_spaces).indexOf(e.target);
+        //gets array item at the index corresponding to the index of the element on the dom
+        const gameboardItem = _gameboard[elementIndex];
+        //if _gameboard[index of element] is empty
+        if (!gameboardItem) {
+            // update _gameboard[] and remove event listener
+            _gameboard[elementIndex] = marker;
+            _render(elementIndex, marker);
+            e.target.removeEventListener('click', _markBoardSpace);
+            console.log(`turn: ${counter++}`)
+        }
+    }
+
+    function _checkGameStatus() {
+
+    }
+
+})();
 
 // factory function
-const Player = (name, score) => {
+const Player = (score, maker) => {
 
-};
+}
